c7ebd6cf7039012d4ab0e322e055459aa34f64a7
asradenblom-sys <asradenblom@gmail.com>
2026-01-02 14:52:27 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 const getImages = function() {
     return "https://raw.githubusercontent.com/asradenblom-sys/CardImages/refs/heads/main/Loading%20Bay.png";
+
+}
+function show(){
+    let image = document.getElementById("1");
+    url = getImages();
+    image.src = url;
 }
